5f728b3d03b180199695a27a157c0681c1653983
prakashtiwari012 <79272146+prakashtiwari012@users.noreply.github.com>
2023-07-18 22:13:22 +0530

    update 2
    update 2

--- app.py
+++ app.py
@@ -5,51 +5,78 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import streamlit_theme as stt
+import streamlit_authenticator as stauth
+import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+with open('auth_config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+#hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 
 # Set page configuration
 st.set_page_config(
     page_title="Wine Dataset Dashboard",
-    page_icon="⚠️",
+    page_icon="🍷",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-# Apply a predefined theme
-#stt.set_theme({'primary': '#701b88'})
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-# Load the Wine dataset
-wine = datasets.load_wine()
-data = pd.DataFrame(wine.data, columns=wine.feature_names)
-data['target'] = wine.target
+name, authentication_status, username = authenticator.login('Login', 'main')
 
-# Set dashboard title
-st.title('Wine Dataset Dashboard')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    
+    # Apply a predefined theme
+    #stt.set_theme({'primary': '#701b88'})
 
-# Sidebar selection
-st.sidebar.title('Explore Wine Dataset')
-selected_feature = st.sidebar.selectbox('Select a feature', wine.feature_names)
-selected_target = st.sidebar.selectbox('Select a target', wine.target_names)
+    # Load the Wine dataset
+    wine = datasets.load_wine()
+    data = pd.DataFrame(wine.data, columns=wine.feature_names)
+    data['target'] = wine.target
 
-# Filter data based on selected feature and target
-filtered_data = data[data['target'] == selected_target][[selected_feature, 'target']]
+    # Set dashboard title
+    st.title('Wine Dataset Dashboard')
 
-# Plotting
-fig = px.scatter(data, x=selected_feature, y='target', color='target', width=800, height=500,
-                 title=f'{selected_feature} vs Target')
-fig.update_traces(marker=dict(size=8))
-fig.update_layout(xaxis_title=selected_feature, yaxis_title='Target')
-st.plotly_chart(fig)
+    # Sidebar selection
+    st.sidebar.title('Explore Wine Dataset')
+    selected_feature = st.sidebar.selectbox('Select a feature', wine.feature_names)
+    selected_target = st.sidebar.selectbox('Select a target', wine.target_names)
 
-# Show the dataset
-st.subheader('Wine Dataset')
-st.dataframe(data)
+    # Filter data based on selected feature and target
+    filtered_data = data[data['target'] == selected_target][[selected_feature, 'target']]
 
-# Show the description of the selected feature
-st.subheader('Description of Selected Feature')
-st.write(wine.feature_names)
-st.write(wine.DESCR.split('\n\n')[1])
+    # Plotting
+    fig = px.scatter(data, x=selected_feature, y='target', color='target', width=800, height=500,
+                    title=f'{selected_feature} vs Target')
+    fig.update_traces(marker=dict(size=8))
+    fig.update_layout(xaxis_title=selected_feature, yaxis_title='Target')
+    st.plotly_chart(fig)
 
-# Show the description of the selected target
-st.subheader('Description of Selected Target')
-st.write(wine.target_names)
-st.write(wine.DESCR.split('\n\n')[2])
+    # Show the dataset
+    st.subheader('Wine Dataset')
+    st.dataframe(data)
+
+    # Show the description of the selected feature
+    st.subheader('Description of Selected Feature')
+    st.write(wine.feature_names)
+    st.write(wine.DESCR.split('\n\n')[1])
+
+    # Show the description of the selected target
+    st.subheader('Description of Selected Target')
+    st.write(wine.target_names)
+    st.write(wine.DESCR.split('\n\n')[2])
+
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
