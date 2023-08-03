b1ed464c339c3bdf55725257fc9339d1cfbc657a
prakashtiwari012 <79272146+prakashtiwari012@users.noreply.github.com>
2023-08-03 18:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,15 @@ def load_image(image_name: str) -> Image:
     """
     return Image.open(image_name)
 
+import streamlit as st
+import base64
+import textwrap
 
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
 
 
 # Set page configuration
